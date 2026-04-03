85b57a20e1320b4d84410ab03a082968f905eb8a
fardeen <fardeenisa@gmail.com>
2026-04-03 15:11:43 +0530

    update run script deployment and cse part inline

--- Project/deploy.ps1
+++ Project/deploy.ps1
@@ -11,9 +11,9 @@ param(
     [string]$vmAdminPassword = "Password123456789"
 )
 
-$securePassword = ConvertTo-SecureString $AzurePassword -AsPlainText -Force
-$credential = New-Object System.Management.Automation.PSCredential($AzureUserName, $securePassword)
-Connect-AzAccount -Credential $credential -TenantId $AzureTenantID -SubscriptionId $AzureSubscriptionID | Out-Null
+$spSecret = ConvertTo-SecureString $AppSecret -AsPlainText -Force
+$spCred = New-Object System.Management.Automation.PSCredential($AppID, $spSecret)
+Connect-AzAccount -ServicePrincipal -Credential $spCred -TenantId $AzureTenantID | Out-Null
 Set-AzContext -SubscriptionId $AzureSubscriptionID | Out-Null
 
 $vmName = "labvm-$DeploymentID"
@@ -26,27 +26,5 @@ Set-Content 'C:\temp\credentials.txt' -Encoding UTF8 -Value @(
     'AppID=$AppID',
     'AppSecret=$AppSecret'
 )
-
-`$creds = @{}
-Get-Content 'C:\temp\credentials.txt' | ForEach-Object {
-    if (`$_ -match '^(.+?)=(.+)$') { `$creds[`$matches[1].Trim()] = `$matches[2].Trim() }
-}
-
-`$spSecret = ConvertTo-SecureString `$creds['AppSecret'] -AsPlainText -Force
-`$spCred = New-Object System.Management.Automation.PSCredential(`$creds['AppID'], `$spSecret)
-Connect-AzAccount -ServicePrincipal -Credential `$spCred -TenantId `$creds['TenantID'] | Out-Null
-Set-AzContext -SubscriptionId `$creds['SubscriptionID'] | Out-Null
-
-`$metadata = Invoke-RestMethod -Uri 'http://169.254.169.254/metadata/instance?api-version=2021-02-01' -Headers @{Metadata='true'}
-`$rg = `$metadata.compute.resourceGroupName
-`$location = `$metadata.compute.location
-
-`$storageName = ('labstorage$DeploymentID').ToLower()
-New-AzStorageAccount -ResourceGroupName `$rg -Name `$storageName -Location `$location -SkuName Standard_LRS -Kind StorageV2 | Out-Null
-
-`$ctx = (Get-AzStorageAccount -ResourceGroupName `$rg -Name `$storageName).Context
-New-AzStorageContainer -Name 'labcontainer' -Context `$ctx -Permission Off | Out-Null
-
-Set-AzStorageBlobContent -File 'C:\temp\hello.txt' -Container 'labcontainer' -Blob 'hello.txt' -Context `$ctx -Force | Out-Null
-Write-Output 'Done'
+& 'C:\temp\run.ps1'
 "
---
+++ Project/run.ps1
@@ -0,0 +1,22 @@
+$creds = @{}
+Get-Content 'C:\temp\credentials.txt' | ForEach-Object {
+    if ($_ -match '^(.+?)=(.+)$') { $creds[$matches[1].Trim()] = $matches[2].Trim() }
+}
+
+$spSecret = ConvertTo-SecureString $creds['AppSecret'] -AsPlainText -Force
+$spCred = New-Object System.Management.Automation.PSCredential($creds['AppID'], $spSecret)
+Connect-AzAccount -ServicePrincipal -Credential $spCred -TenantId $creds['TenantID'] | Out-Null
+Set-AzContext -SubscriptionId $creds['SubscriptionID'] | Out-Null
+
+$metadata = Invoke-RestMethod -Uri 'http://169.254.169.254/metadata/instance?api-version=2021-02-01' -Headers @{Metadata='true'}
+$rg = $metadata.compute.resourceGroupName
+$location = $metadata.compute.location
+
+$storageName = ('labstorage' + $creds['SubscriptionID'].Substring(0,8)).ToLower() -replace '[^a-z0-9]',''
+New-AzStorageAccount -ResourceGroupName $rg -Name $storageName -Location $location -SkuName Standard_LRS -Kind StorageV2 | Out-Null
+
+$ctx = (Get-AzStorageAccount -ResourceGroupName $rg -Name $storageName).Context
+New-AzStorageContainer -Name 'labcontainer' -Context $ctx -Permission Off | Out-Null
+
+Set-AzStorageBlobContent -File 'C:\temp\hello.txt' -Container 'labcontainer' -Blob 'hello.txt' -Context $ctx -Force | Out-Null
+Write-Output 'Done'
