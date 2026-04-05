1e3ebc8aa395c96405c45f838f37744923c854a3
fardeen <fardeenisa@gmail.com>
2026-04-05 10:53:42 +0530

    updated run and azuredeploy

--- Project/run.ps1
+++ Project/run.ps1
@@ -3,44 +3,39 @@ Get-Content 'C:\temp\credentials.txt' | ForEach-Object {
     if ($_ -match '^(.+?)=(.+)$') { $creds[$matches[1].Trim()] = $matches[2].Trim() }
 }
 
-$spSecure = ConvertTo-SecureString $creds['AppSecret'] -AsPlainText -Force
-$spCred = New-Object System.Management.Automation.PSCredential($creds['AppID'], $spSecure)
-Connect-AzAccount -ServicePrincipal -Credential $spCred -TenantId $creds['TenantID'] | Out-Null
-Set-AzContext -SubscriptionId $creds['SubscriptionID'] | Out-Null
+$securePassword = ConvertTo-SecureString $creds['AzurePassword'] -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential($creds['AzureUserName'], $securePassword)
+Connect-AzAccount -Credential $credential -TenantId $creds['TenantID'] | Out-Null
+
+$SubscriptionID = (Get-AzContext).Subscription.Id
+
+$sp = New-AzADApplication -DisplayName "LabSPN-$(Get-Random -Minimum 1000 -Maximum 9999)"
+New-AzADServicePrincipal -ApplicationId $sp.AppId | Out-Null
+$spSecret = New-AzADAppCredential -ApplicationId $sp.AppId
+$NewSPNAppID = $sp.AppId
+$NewSPNSecret = $spSecret.SecretText
 
 $metadata = Invoke-RestMethod -Uri 'http://169.254.169.254/metadata/instance?api-version=2021-02-01' -Headers @{Metadata='true'}
 $rg = $metadata.compute.resourceGroupName
-$location = $metadata.compute.location
 
-$storageName = ('labstorage' + $creds['SubscriptionID'].Substring(0,8)).ToLower() -replace '[^a-z0-9]',''
+New-AzRoleAssignment -ApplicationId $NewSPNAppID -RoleDefinitionName "Owner" -ResourceGroupName $rg | Out-Null
+
+Add-Content 'C:\temp\credentials.txt' -Value "AppID=$NewSPNAppID"
+Add-Content 'C:\temp\credentials.txt' -Value "AppSecret=$NewSPNSecret"
+
+Start-Sleep -Seconds 30
+
+$spSecure = ConvertTo-SecureString $NewSPNSecret -AsPlainText -Force
+$spCred = New-Object System.Management.Automation.PSCredential($NewSPNAppID, $spSecure)
+Connect-AzAccount -ServicePrincipal -Credential $spCred -TenantId $creds['TenantID'] | Out-Null
+Set-AzContext -SubscriptionId $SubscriptionID | Out-Null
+
+$location = $metadata.compute.location
+$storageName = "labstorage" + (Get-Random -Minimum 10000 -Maximum 99999)
 New-AzStorageAccount -ResourceGroupName $rg -Name $storageName -Location $location -SkuName Standard_LRS -Kind StorageV2 | Out-Null
 
 $ctx = (Get-AzStorageAccount -ResourceGroupName $rg -Name $storageName).Context
 New-AzStorageContainer -Name 'labcontainer' -Context $ctx -Permission Off | Out-Null
 
 Set-AzStorageBlobContent -File 'C:\temp\hello.txt' -Container 'labcontainer' -Blob 'hello.txt' -Context $ctx -Force | Out-Null
-Write-Output 'Done'
-
-
-# $creds = @{}
-# Get-Content 'C:\temp\credentials.txt' | ForEach-Object {
-#     if ($_ -match '^(.+?)=(.+)$') { $creds[$matches[1].Trim()] = $matches[2].Trim() }
-# }
-
-# $spSecure = ConvertTo-SecureString $creds['AppSecret'] -AsPlainText -Force
-# $spCred = New-Object System.Management.Automation.PSCredential($creds['AppID'], $spSecure)
-# Connect-AzAccount -ServicePrincipal -Credential $spCred -TenantId $creds['TenantID'] | Out-Null
-# Set-AzContext -SubscriptionId $creds['SubscriptionID'] | Out-Null
-
-# $metadata = Invoke-RestMethod -Uri 'http://169.254.169.254/metadata/instance?api-version=2021-02-01' -Headers @{Metadata='true'}
-# $rg = $metadata.compute.resourceGroupName
-# $location = $metadata.compute.location
-
-# $storageName = ('labstorage' + $creds['SubscriptionID'].Substring(0,8)).ToLower() -replace '[^a-z0-9]',''
-# New-AzStorageAccount -ResourceGroupName $rg -Name $storageName -Location $location -SkuName Standard_LRS -Kind StorageV2 | Out-Null
-
-# $ctx = (Get-AzStorageAccount -ResourceGroupName $rg -Name $storageName).Context
-# New-AzStorageContainer -Name 'labcontainer' -Context $ctx -Permission Off | Out-Null
-
-# Set-AzStorageBlobContent -File 'C:\temp\hello.txt' -Container 'labcontainer' -Blob 'hello.txt' -Context $ctx -Force | Out-Null
-# Write-Output 'Done'
+Write-Output "Done: $storageName | labcontainer | hello.txt uploaded"
