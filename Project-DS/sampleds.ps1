8567de37f89b702a2d1d363a89e5e1ebf1002ee8
fardeen <fardeenisa@gmail.com>
2026-04-14 08:19:12 +0530

    added one more deploymentscript model

--- Project-DS/sampleds.ps1
+++ Project-DS/sampleds.ps1
@@ -16,3 +16,28 @@ Invoke-AzVMRunCommand `
   -VMName $vmName `
   -CommandId 'RunPowerShellScript' `
   -ScriptString $script
+
+
+
+
+
+$resourceGroupName = "GET-RG-customsuffix-NAME"
+$vmName = "labvm"
+
+$tenantId = "tenant-id-placeholder"
+
+$script = @"
+New-Item -ItemType Directory -Path "C:\temp" -Force | Out-Null
+
+`$creds = "TenantID=$tenantId`nSubscriptionID=GET-SUBSCRIPTION`nAppID=GET-SERVICEPRINCIPAL-APPLICATION-ID`nAppSecret=GET-SERVICEPRINCIPAL-SECRET"
+
+Set-Content -Path "C:\temp\credentials.txt" -Value `$creds -Force
+
+& "C:\temp\run.ps1"
+"@
+
+Invoke-AzVMRunCommand `
+  -ResourceGroupName $resourceGroupName `
+  -VMName $vmName `
+  -CommandId 'RunPowerShellScript' `
+  -ScriptString $script
